d4696ca46d84321a6fad258091cc3c3bd2377a3f
Sebastian Llanes <sebastianllanessaavedra@gmail.com>
2024-05-31 14:04:14 -0300

    Change place of img. src to public

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -12,7 +12,9 @@ const SingleItem = ( { price, type, id, eliminarItem, editItem } ) => {
         eliminarItem(id);
         }     
     };
-    const urlImage = `/src/assets/${type}.png`;
+    // const urlImage = `/src/assets/${type}.png`;
+    const urlImage = `/assets/${type}.png`;
+
     return (
         <div className="single-item">
             <img src={ urlImage } alt="Services" />
